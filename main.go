b417b88303ed77195b0aaf7d67788f20bbd0110b
StudentOfJS <rodlewis45@gmail.com>
2018-08-29 14:05:40 +0200

    add book

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/asdine/storm"
+	uuid "github.com/satori/go.uuid"
 
 	"github.com/gorilla/mux"
 )
@@ -26,6 +27,7 @@ func getBooks(w http.ResponseWriter, r *http.Request) {
 	db, err := storm.Open("books.db")
 	if err != nil {
 		log.Fatal(err)
+		return
 	}
 	defer db.Close()
 	var books []book
@@ -35,10 +37,31 @@ func getBooks(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(books)
+	return
+}
+
+func addBook(w http.ResponseWriter, r *http.Request) {
+	db, err := storm.Open("books.db")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer db.Close()
+	w.Header().Set("Content-Type", "application/json")
+	var book book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = uuid.Must(uuid.NewV4()).String()
+	if err := db.Save(&book); err != nil {
+		log.Fatal(err)
+		return
+	}
+	json.NewEncoder(w).Encode(book)
+	return
 }
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/addbook", addBook).Methods("POST")
 
 }
