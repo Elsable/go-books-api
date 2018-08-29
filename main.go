a4e912e92036f4c694723077bcd4daecfde1a9a1
StudentOfJS <rodlewis45@gmail.com>
2018-08-29 13:51:28 +0200

    add books db

--- main.go
+++ main.go
@@ -2,26 +2,37 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+
+	"github.com/asdine/storm"
 
 	"github.com/gorilla/mux"
 )
 
 type book struct {
-	ID     string  `json:"id"`
-	Isbn   string  `json:"isbn"`
-	Title  string  `json:"title"`
-	author *author `json:"author"`
+	ID     string  `storm:"id"`
+	Isbn   string  `storm:"unique"`
+	Title  string  `storm:"index"`
+	Author *author `storm:"inline"`
 }
 
 type author struct {
-	Firstname string `json:"firstname"`
-	Lastname  string `json:"lastname"`
+	Firstname string `storm:"index"`
+	Lastname  string `storm:"index"`
 }
 
-var books []book
-
 func getBooks(w http.ResponseWriter, r *http.Request) {
+	db, err := storm.Open("books.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	var books []book
+	if err := db.All(&books); err != nil {
+		log.Fatal(err)
+		return
+	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(books)
 }
